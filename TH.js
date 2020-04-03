5d51bd3027a055318288d17be63ef2017d6325e1
nqcuong20 <60700939+nqcuong20@users.noreply.github.com>
2020-04-03 22:52:41 +0700

    Martin Nguyen

---
+++ TH.js
@@ -0,0 +1,13 @@
+ $(function(){
+     $(window).scroll(function(){
+        
+        {
+            $('.logo').addClass('maucam')
+            $('.navbar').addClass('nentrang')
+            .removeClass('navbar-dark')
+            .addClass('navbar-light')
+        }
+         
+
+     })
+ })
